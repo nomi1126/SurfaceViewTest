07e803eec90bb9b5c974c106150e30cfeae2f17b
ryota niinomi <ryotakosu@gmail.com>
2014-07-20 23:55:32 +0900

    Refactor

--- app/src/main/java/com/example/nomiyan/surfaceviewtest/Ball.java
+++ app/src/main/java/com/example/nomiyan/surfaceviewtest/Ball.java
@@ -12,33 +12,33 @@ public class Ball {
 
     public Ball(int r, int x, int y, int v, int _dirX, int _dirY) {
         Random random = new Random();
-        this.radius   = r;
-        this.posX     = x;
-        this.posY     = y;
-        this.velocity = v;
-        this.dirX     = _dirX;
-        this.dirY     = _dirY;
-        this.color    = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        radius   = r;
+        posX     = x;
+        posY     = y;
+        velocity = v;
+        dirX     = _dirX;
+        dirY     = _dirY;
+        color    = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
     }
 
     public void setPosX(int x) {
-        this.posX += x;
+        posX += x;
     }
 
     public void setPosY(int y) {
-        this.posY += y;
+        posY += y;
     }
 
     public void updateDirX() {
-        this.dirX *= -1;
+        dirX *= -1;
     }
 
     public void updateDirY() {
-        this.dirY *= -1;
+        dirY *= -1;
     }
 
     public void setColor() {
         Random random = new Random();
-        this.color = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        color = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
     }
 }
